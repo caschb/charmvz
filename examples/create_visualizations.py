0d26de310fef35d8d229d5769668b313089fdb50
Christian Asch <christian.asch4@outlook.com>
2025-06-03 13:26:09 -0300

    Added an id to differenciate plots between jobs

--- examples/create_visualizations.py
+++ examples/create_visualizations.py
@@ -6,8 +6,8 @@ from charmvz.analysis import visualization
 
 
 def main():
-    if len(argv) != 2:
-        print(f"Correct usage: f{argv[0]} <parquet filename>.pq")
+    if len(argv) != 3:
+        print(f"Correct usage: f{argv[0]} <parquet filename>.pq <jobid>")
         exit(-1)
 
     print("Reading parquet file..")
@@ -27,13 +27,13 @@ def main():
 
     df = df.rename({"Value": "Chare", "Parent": "Processing Element"})
     df = df.filter(pl.col.Duration > 0.0)
-
+    jobid = argv[2]
     print("Creating frequency plot..")
-    visualization.create_frequency_plot(df, filename=f"freq_plot_{argv[1].split("_")[-1].split(".")[0]}.png")
+    visualization.create_frequency_plot(df, filename=f"freq_plot_{jobid}.png")
     print("Creating total duration plot..")
-    visualization.create_total_duration_plot(df)
+    visualization.create_total_duration_plot(df, filename=f"duration_plot_{jobid}.png")
     print("Creating activity per pe heatmap..")
-    visualization.create_chare_activity_per_pe_heatmap(df)
+    visualization.create_chare_activity_per_pe_heatmap(df, filename=f"chare_activity_plot_{jobid}.png")
 
 if __name__ == "__main__":
     main()
